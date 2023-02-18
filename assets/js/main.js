6c00eec11ebeb1f5ed05ff2fae37ebb28a97ca5c
yasmeenbsharat <y.bsharat@student.aaup.edu>
2023-02-18 12:15:40 +0200

    edit project

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,3 +230,23 @@ window.addEventListener("load",function(){
        body.style.overflow='auto';
     },2000);
 });
+
+$(document).ready(function(){
+    
+    $('.carousel_01').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
